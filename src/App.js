9e6bc52b126b86c11d1218ef1bf1a6238eea63f6
kichyr <kichyr@mail.ru>
2019-12-02 16:12:30 +0300

    in some way react route works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import DialogList from './components/DialogList/DialogList';
 import MessageForm from './components/DialogPage/MessageForm';
 import ProfilePage from './components/ProfilePage/ProfilePage'
 import './styles/globalStyles.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	withRouter
+} from "react-router-dom";
+import { match } from 'minimatch';
 
 
 
-function App() {
+const App = withRouter((props) => {
 	const [appState, setAppState] = useState( 
 		{
-			appPage: 'ChatList',
+			appPage: '', // Chat, ProfilePage
 			prevAppPage: 'ChatList',
 			openedChat: {
 				chatId: -1,
@@ -18,6 +26,49 @@ function App() {
 			}
 		}
 	);
+useEffect(()=>{
+	switch (props.location.pathname) {
+		case '/':
+			if (appState.appPage !== 'ChatList')
+				setAppState({...appState, prevAppPage: appState.appPage, appPage: 'ChatList'});
+			break;
+
+		case '/profile':
+			if (appState.appPage !== 'ProfilePage')
+				setAppState({...appState, prevAppPage: appState.appPage, appPage: 'ProfilePage'});
+			break;
+			
+		case '/chat':
+			if (appState.appPage !== 'Chat')
+				setAppState({...appState, prevAppPage: appState.appPage, appPage: 'Chat'});
+			break;	
+		default:
+			break;
+	}
+},[]);
+
+	useEffect(
+		()=>{
+			switch (appState.appPage) {
+				case 'ProfilePage':
+					props.history.replace('profile');
+					break;
+				
+				case 'ChatList':
+					props.history.push('/');
+					break;
+			
+				case 'Chat':
+					props.history.replace('/chat');
+					break;
+				default:
+					break;
+			}
+			
+		},
+		[appState.appPage]
+	);
+	console.log(appState.appPage + appState.appPage);
 	return (
 		<React.Fragment>
 			<MessageForm appState={appState} setAppState={setAppState}/> 
@@ -25,5 +76,18 @@ function App() {
 			<ProfilePage appState={appState} setAppState={setAppState} />
 		</React.Fragment>
 	);
-}
-export default App;
+});
+
+const RouteApp = (() => {
+	return (
+		<Router>
+			<Switch>
+				<Route path='/(profile||Chat)' 
+					component={App}
+				/> 
+			</Switch>
+		</Router>
+	);
+});
+
+export default RouteApp;
---
+++ src/components/BrowserHistory.js
@@ -0,0 +1,4 @@
+
+const BrowserHistory = require('react-router/lib/BrowserHistory').default;
+
+export default BrowserHistory;
