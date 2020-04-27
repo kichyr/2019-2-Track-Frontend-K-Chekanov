b896004ac500e81bade71f2a705a60fadd3ab5c2
kichyr <kichyr@mail.ru>
2020-04-27 16:42:47 +0300

    some work on d3

---
+++ ts2/src/map/RussiaMap.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react'
+import * as DataDef from './utils'
+import * as d3 from 'd3'
+
+// Resolves color by country name
+async function getColorFunction(): Promise<(name: string) => string> {
+  const covidSummary: DataDef.ICOVIDStatistic = await DataDef.getCOVIDStatistic()
+
+  let maxConfirmed = 0
+  covidSummary.Countries.forEach((country: DataDef.ICOVDCountryInfo) => {
+    if (country.TotalConfirmed > maxConfirmed) {
+      maxConfirmed = country.TotalConfirmed
+    }
+  })
+
+  return (name: string): string => {
+    for (const country of covidSummary.Countries) {
+      if (name === 'USA') name = 'United States of America'
+      if (country.Country.toLowerCase() === name.toLowerCase() || country.Slug.toLowerCase() === name.toLowerCase()) {
+        console.log(country.TotalConfirmed)
+        console.log(maxConfirmed)
+        console.log(`rgb(${Math.floor((255 * country.TotalConfirmed) / maxConfirmed)}, 0, 0)`)
+        return `rgb(${Math.floor(255 * Math.pow(country.TotalConfirmed / maxConfirmed, 1 / 5))}, 0, 0)`
+      }
+    }
+    return `rgb(0, 0, 0)`
+  }
+}
+
+async function generatePath(
+  svg: d3.Selection<d3.BaseType, any, d3.BaseType, any>,
+  countries: DataDef.GeoJson,
+  path: d3.ValueFn<SVGPathElement, unknown, string | number | boolean | null>,
+) {
+  const colorResolver = await getColorFunction()
+
+  svg
+    .selectAll('path')
+    .data(countries.features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .style('fill', (d: any) => {
+      console.log(`${colorResolver(d.properties.name)}`)
+      return `${colorResolver(d.properties.name)}`
+    })
+  /*.on("mouseover",function(d) {
+    	console.log("just had a mouseover", d);
+    	d3.select(this)
+      	.classed("active",true)
+  	})
+  	.on("mouseout",function(d){
+    	d3.select(this)
+      	.classed("active",false)
+    }); */
+}
+
+type GlobeProps = {
+  height: number
+  width: number
+  geoJson: DataDef.GeoJson
+}
+
+function Globe({ height, width, geoJson }: GlobeProps) {
+  let projection = d3
+    .geoEqualEarth()
+    .scale(height * 2)
+    .center(d3.geoPath().centroid(geoJson as d3.GeoPermissibleObjects))
+    .clipAngle(0)
+    .fitSize([width, height], (geoJson as unknown) as d3.ExtendedGeometryCollection<d3.GeoGeometryObjects>)
+
+  let geoGenerator = d3.geoPath().projection(projection)
+  let svgRef: any = React.createRef<SVGAElement>()
+
+  useEffect(() => {
+    generatePath(d3.select(svgRef.current), geoJson, geoGenerator as any)
+  })
+
+  return <svg ref={svgRef} className={'node'} width={width} height={height}></svg>
+}
+
+export default Globe
