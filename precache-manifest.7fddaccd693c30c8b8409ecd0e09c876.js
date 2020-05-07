0c8b2025038a2627e29a3798e3dd6c6d38b2c6b8
kichyr <kichyr@mail.ru>
2020-05-07 20:57:44 +0300

    Updates

---
+++ precache-manifest.7fddaccd693c30c8b8409ecd0e09c876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165d37663a3b1fa51cf722040154547f",
+    "url": "/2019-2-Track-Frontend-K-Chekanov/index.html"
+  },
+  {
+    "revision": "47a185686d053b9ee316",
+    "url": "/2019-2-Track-Frontend-K-Chekanov/static/css/main.a719e54b.chunk.css"
+  },
+  {
+    "revision": "68e4d45a2b2beee3b4c7",
+    "url": "/2019-2-Track-Frontend-K-Chekanov/static/js/2.00a52b1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2019-2-Track-Frontend-K-Chekanov/static/js/2.00a52b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a185686d053b9ee316",
+    "url": "/2019-2-Track-Frontend-K-Chekanov/static/js/main.536c6b0f.chunk.js"
+  },
+  {
+    "revision": "90632de91b1dc534f5bd",
+    "url": "/2019-2-Track-Frontend-K-Chekanov/static/js/runtime-main.2459e61c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdda56d335772965cc535e6382103102.js"
+  "/2019-2-Track-Frontend-K-Chekanov/precache-manifest.7fddaccd693c30c8b8409ecd0e09c876.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2019-2-Track-Frontend-K-Chekanov/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
